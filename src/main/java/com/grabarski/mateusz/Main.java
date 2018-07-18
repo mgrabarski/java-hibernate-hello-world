1f8482b67faf6faba57a4c8966eda453f7d10e4a
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2018-07-18 18:44:55 +0200

    created first query

--- src/main/java/com/grabarski/mateusz/Main.java
+++ src/main/java/com/grabarski/mateusz/Main.java
@@ -1,7 +1,23 @@
 package com.grabarski.mateusz;
 
+import com.grabarski.mateusz.domain.models.City;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
 /**
  * Created by Mateusz Grabarski on 18.07.2018.
  */
 public class Main {
+
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+
+        Query<City> query = session.createQuery("FROM City");
+        query.setMaxResults(10);
+
+        query.getResultStream().forEach(city -> System.out.println(city));
+    }
 }
