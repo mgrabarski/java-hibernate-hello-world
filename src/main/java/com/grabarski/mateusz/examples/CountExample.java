57f11cc922ed90e2e90609b1edea41debe99e4d8
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2018-07-19 19:02:05 +0200

    Added example with count

---
+++ src/main/java/com/grabarski/mateusz/examples/CountExample.java
@@ -0,0 +1,26 @@
+package com.grabarski.mateusz.examples;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+/**
+ * Created by Mateusz Grabarski on 19.07.2018.
+ */
+public class CountExample {
+
+    public static void main(String[] args) {
+        try (SessionFactory factory = new Configuration().configure().buildSessionFactory();
+             Session session = factory.openSession()) {
+
+            Query<Object[]> query = session.createQuery(
+                    "SELECT c.name, c.countryLanguages.size " +
+                            "FROM Country c");
+
+            query.setMaxResults(30);
+
+            query.stream().forEach(objects -> System.out.println("Name: " + objects[0] + ", number = " + objects[1]));
+        }
+    }
+}
